0a87b887e2e5dfeeb61b90d485d3f2f8f8369a34
virat2110 <83300897+virat2110@users.noreply.github.com>
2022-12-14 20:07:10 +0530

    initial commit

---
+++ Eureka/src/main/java/com/virat/admin/EurekaApplication.java
@@ -0,0 +1,25 @@
+package com.virat.admin;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
---
+++ GatewayAirline/src/main/java/com/virat/demo/GatewayAirlineApplication.java
@@ -0,0 +1,13 @@
+package com.virat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAirlineApplication.class, args);
+	}
+
+}
